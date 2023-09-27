466057c097bce1f83175c729221659d7fcedfd29
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-09-27 19:36:19 +0300

    queries for database are written

--- expressMiddleware/controllers/appointmentShopController.js
+++ expressMiddleware/controllers/appointmentShopController.js
@@ -1,2 +1,24 @@
 const uuid = require('uuid');
 const client = require('./db')
+
+exports.getShopAppointments = async (req, res) => {
+  const { shop_id,when_0,when_1 } = req.body;
+  console.log(req.body);
+  const shopAppointments = await client.execute(
+    'select * from appointment where shop_id = ? and (when<=? and when>=?)',// 8a allaksei se or phone
+    [shop_id,when_0,when_1]
+  );
+  const appointments = shopAppointments.rows
+  res.json({appointments});
+};
+
+exports.getEmployeeAppointments = async (req, res) => {
+  const { shop_id,employee_email,when_0,when_1 } = req.body;
+  console.log(req.body);
+  const shopAppointments = await client.execute(
+    'select * from appointment where shop_id=? and employee_email=? and (when<=? and when>=?);',
+    [shop_id,employee_email,when_0,when_1]
+  );
+  const appointments = shopAppointments.rows
+  res.json({appointments});
+};
